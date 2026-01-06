7ebc3df363d099ce117e900b3d817236680fbe34
jhk01007 <jhk001007@naver.com>
2026-01-07 03:44:20 +0900

    refactor(RecommendedMenu): RecommendedMenu로 분리

--- src/main/java/menu/domain/MenuRecommend.java
+++ src/main/java/menu/domain/MenuRecommend.java
@@ -1,17 +1,16 @@
 package menu.domain;
 
 import java.util.List;
-import java.util.Map;
 
 public class MenuRecommend {
 
     private DayOfTheWeek dayOfTheWeek;
     private MenuCategory menuCategory;
-    private List<Map<Coach, Menu>> recommendedMenu;
+    private List<RecommendedMenu> recommendedMenuList;
 
-    public MenuRecommend(DayOfTheWeek dayOfTheWeek, MenuCategory menuCategory, List<Map<Coach, Menu>> recommendedMenu) {
+    public MenuRecommend(DayOfTheWeek dayOfTheWeek, MenuCategory menuCategory, List<RecommendedMenu> recommendedMenuList) {
         this.dayOfTheWeek = dayOfTheWeek;
         this.menuCategory = menuCategory;
-        this.recommendedMenu = recommendedMenu;
+        this.recommendedMenuList = recommendedMenuList;
     }
 }
--- src/main/java/menu/service/MenuService.java
+++ src/main/java/menu/service/MenuService.java
@@ -1,7 +1,11 @@
 package menu.service;
 
+import menu.domain.Coach;
 import menu.domain.Menu;
+import menu.domain.MenuRecommend;
 import menu.repo.MenuRepository;
+
+import java.util.List;
 
 import static menu.util.ErrorMessage.MENU_NOT_FOUNT_ERROR;
 
@@ -16,5 +20,17 @@ public class MenuService {
     public Menu findMenuByName(String name) {
         return menuRepository.findByName(name)
                 .orElseThrow(() -> new IllegalArgumentException(MENU_NOT_FOUNT_ERROR.getMessage()));
+    }
+
+    public List<MenuRecommend> recommendMenu(List<Coach> coachList) {
+
+        // 각 요일별로 카테고리 선정
+
+        // 한주에 해당 카테고리가 2회이상 나왔는지 검증
+
+        // 각 코치별로 해당 카테고리의 메뉴 선정
+
+        // 해당 코치가 해당 메뉴를 먹은적이 있는지 검증
+        return null;
     }
 }
