ca0da72908d1884c85a8d7d9d971edf6f1f69424
jhk01007 <jhk001007@naver.com>
2026-01-07 04:31:00 +0900

    feat(Coach): 코치가 해당 메뉴를 먹을 수 있는지 여부를 반환하는 로직 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -5,10 +5,26 @@ import java.util.List;
 public class Coach {
 
     private String name;
-    private List<Menu> cantEatMenu;
+    private List<Menu> cantEatMenuList;
 
-    public Coach(String name, List<Menu> cantEatMenu) {
+    public Coach(String name, List<Menu> cantEatMenuList) {
         this.name = name;
-        this.cantEatMenu = cantEatMenu;
+        this.cantEatMenuList = cantEatMenuList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 해당 메뉴를 먹을 수 있는지 여부
+     */
+    public boolean canEat(Menu menu) {
+        for (Menu cantEatMenu : cantEatMenuList) {
+            if(cantEatMenu.equals(menu)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ src/test/java/menu/domain/CoachTest.java
@@ -0,0 +1,20 @@
+package menu.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoachTest {
+
+    @Test
+    @DisplayName("코치가 해당 메뉴를 먹을 수 있는지 여부를 반환한다.")
+    public void canEat() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
