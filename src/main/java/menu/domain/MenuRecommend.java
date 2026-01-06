e679e9e18af40d368be4d5a0d91763990189f751
jhk01007 <jhk001007@naver.com>
2026-01-07 04:16:47 +0900

    feat(MenuRecommend): 이미 해당 코치가 해당 메뉴를 추천받은적이 있는지 여부를 반환하는 메서드 구현

--- src/main/java/menu/domain/MenuRecommend.java
+++ src/main/java/menu/domain/MenuRecommend.java
@@ -13,4 +13,21 @@ public class MenuRecommend {
         this.menuCategory = menuCategory;
         this.recommendedMenuList = recommendedMenuList;
     }
+
+    public DayOfTheWeek getDayOfTheWeek() {
+        return dayOfTheWeek;
+    }
+
+    public MenuCategory getMenuCategory() {
+        return menuCategory;
+    }
+
+    public boolean isAlreadyRecommended(Menu menu, Coach coach) {
+        for (RecommendedMenu recommendedMenu : recommendedMenuList) {
+            if(recommendedMenu.isSameRecommend(menu, coach)) { // 이미 해당 코치가 해당 메뉴를 추천 받은적이 있으면
+                return true;
+            }
+        }
+        return false;
+    }
 }
---
+++ src/test/java/menu/domain/MenuRecommendTest.java
@@ -0,0 +1,20 @@
+package menu.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuRecommendTest {
+
+    @Test
+    @DisplayName("이미 해당코치가 해당 메뉴를 추천받은적이 있는지 여부를 반환한다.")
+    public void isAlreadyRecommended() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
