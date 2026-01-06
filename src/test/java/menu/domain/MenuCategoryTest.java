0c942efd92a98581ba335612d8dd6b07393f5bf6
jhk01007 <jhk001007@naver.com>
2026-01-07 06:09:42 +0900

    test(MenuCategory): nameOf, numberOf 테스트 작성

--- src/test/java/menu/domain/MenuCategoryTest.java
+++ src/test/java/menu/domain/MenuCategoryTest.java
@@ -1,30 +1,48 @@
 package menu.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MenuCategoryTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {
+            "일식,JAPANESE",
+            "한식,KOREAN",
+            "중식,CHINESE",
+            "아시안,ASIAN",
+            "양식,WESTERN"
+    })
     @DisplayName("이름으로 MenuCategory를 찾는다.")
-    public void nameOf() throws Exception {
-        // given
+    public void nameOf(String menuName, MenuCategory menuCategory) throws Exception {
 
         // when
+        MenuCategory result = MenuCategory.nameOf(menuName);
 
         // then
+        assertThat(result).isEqualTo(menuCategory);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,JAPANESE",
+            "2,KOREAN",
+            "3,CHINESE",
+            "4,ASIAN",
+            "5,WESTERN"
+    })
     @DisplayName("번호로 MenuCategory를 찾는다.")
-    public void numberOf() throws Exception {
-        // given
-
+    public void numberOf(int number, MenuCategory menuCategory) throws Exception {
         // when
+        MenuCategory result = MenuCategory.numberOf(number);
 
         // then
+        assertThat(result).isEqualTo(menuCategory);
     }
 
 }
