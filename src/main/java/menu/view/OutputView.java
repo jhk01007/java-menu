5a0ed3414814fb6ee9e076b22d244b86ccfdd150
jhk01007 <jhk001007@naver.com>
2026-01-07 02:19:12 +0900

    feat( OutputView): 메뉴 추천 시작 메시지 출력 로직 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,13 @@
+package menu.view;
+
+public class OutputView {
+
+
+    public void printStartMessage() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public void printError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+}
